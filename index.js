ba56af95338d5d020bef7eb8f22da2967efe84f8
jacobaranza <jacobthomasjunior@gmail.com>
2023-04-02 16:23:10 -0500

    Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+// Question 1
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply); // Output: 62
 
-// Write your code here
+// Question 2
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random); // Output: a random integer between 1 and 100
+
+// Question 3
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+console.log(mod); // Output: 4
+
+// Question 4
+let numbers = [1, 5, 20, 10, 7];
+let max = Math.max(...numbers);
+console.log(max); // Output: 20
